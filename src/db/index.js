a2387ffc3c5002c0f55d2ce6c6bee1d8188beaf7
G-Chilie <okonkwolinda7@gmail.com>
2019-10-31 14:42:08 +0100

    database setup

---
+++ src/db/index.js
@@ -0,0 +1,66 @@
+[
+    {
+    id: 1,
+    code: "AFR",
+    company: "AFRIPRUD",
+    price:   1.68,
+    volume: 100
+  },
+  {
+    id: 2,
+    code: "STA",
+    company: "STANBIC",
+    price:   19.69,
+    volume: 100
+  },
+  {
+    id: 3,
+    code: "UNC",
+    company: "UNIC[MRF]",
+    price:   0.11,
+    volume: 100
+  },
+  {
+    id: 4,
+    code: "VIR",
+    company: "VERITASKAP",
+    price:   0.10,
+    volume: 100
+  },
+  {
+    id: 5,
+    code: "CAV",
+    company: "CAVERTON[BLS]",
+    price:   1.68,
+    volume: 100
+  },
+  {
+    id: 6,
+    code: "AIR",
+    company: "AIRTELAFRI",
+    price:   229.52,
+    volume: 100
+  },
+  {
+    id: 7,
+    code: "AFR",
+    company: "AFRIPRUD",
+    price:   1.68,
+    volume: 100
+  },
+  {
+    id: 8,
+    code: "STA",
+    company: "STANBIC",
+    price:   19.69,
+    volume: 100
+  },
+  {
+    id: 9,
+    code: "AFR",
+    company: "AFRIPRUD",
+    price:   1.68,
+    volume: 100
+  }
+
+  ]
